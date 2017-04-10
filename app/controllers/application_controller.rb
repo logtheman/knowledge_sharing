eed8ede68c4df84b3d6cb7efb2ee3bfc60b504b8
Logan Murdock <logtheman@gmail.com>
2017-04-10 13:36:46 -0700

    saving before react

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,11 @@ class ApplicationController < ActionController::Base
    def configure_permitted_parameters
      devise_parameter_sanitizer.permit(:login, keys: [:username, :email, :password, :remember_me])
      devise_parameter_sanitizer.permit(:sign_up) do |u|
-       u.permit(:name, :username, :image,
+       u.permit(:name, :username, :first_name, :last_name,
          :email, :password, :password_confirmation)
+      devise_parameter_sanitizer.permit(:edit) do |u|
+        u.permit(:name, :username, :first_name, :last_name,
+          :email, :password, :password_confirmation)
      end
    end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,25 +14,7 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
 
 
-  # def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
-  #     data = access_token.info
-  #     user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
-  #     if user
-  #       return user
-  #     else
-  #       registered_user = User.where(:email => access_token.info.email).first
-  #       if registered_user
-  #         return registered_user
-  #       else
-  #         user = User.create(name: data["name"],
-  #           provider:access_token.provider,
-  #           email: data["email"],
-  #           uid: access_token.uid ,
-  #           password: Devise.friendly_token[0,20],
-  #         )
-  #       end
-  #    end
-  # end
+
 
   def self.from_omniauth(access_token)
       data = access_token.info
@@ -42,6 +24,8 @@ class User < ApplicationRecord
       unless user
           # Strip out the username form email
           user = User.create(username: data["email"].split("@")[0],
+             first_name: data["name"].split(" ")[0],
+             last_name: data["name"].split(" ")[0],
              email: data["email"],
              password: Devise.friendly_token[0,20]
           )
