4d0f9e904eb57e8956874061d444d70d8478f46f
Logan Murdock <logtheman@gmail.com>
2017-04-15 08:57:39 -0700

    added-end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
      devise_parameter_sanitizer.permit(:sign_up) do |u|
        u.permit(:name, :username, :first_name, :last_name,
          :email, :password, :password_confirmation)
+      end
       devise_parameter_sanitizer.permit(:edit) do |u|
         u.permit(:name, :username, :first_name, :last_name,
           :email, :password, :password_confirmation)
